7f3f23671b1daceaaa16562703782e5609746cca
adela bodea <adebodea@gmail.com>
2019-05-22 18:09:14 +0200

    rails 3-2

---
+++ db/migrate/20190522113412_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phonenumber
+      t.string :category
+      t.text :review
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 restaurants'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Faker::Restaurant.type,
+    review: Faker::Restaurant.review
+    )
+  restaurant.save!
+
+end
+
+# puts 'Cleaning database...'
+# Restaurant.destroy_all
+
+# puts 'Creating restaurants...'
+# restaurants_attributes[
+#   {
+#     name:         'Dishoom',
+#     address:      '7 Boundary St, London E2 7JE',
+#     description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+#     stars:        5
+#   },
+#   {
+#     name:         'Pizza East',
+#     address:      '56A Shoreditch High St, London E1 6PQ',
+#     description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+#     stars:        4
+#   }
+# ]
+# Restaurant.create!(restaurants_attributes)
+# puts 'Finished!'
+
+
+
